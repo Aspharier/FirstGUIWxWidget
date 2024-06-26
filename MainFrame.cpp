53434770a4d90390d17d0c3728cf584ca05d3c30
Aspharier <codeworkashish@gmail.com>
2024-06-26 16:29:00 +0530

    Added Dynamic Event Handling

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -2,24 +2,31 @@
 #include <wx/wx.h>
 
 
-enum IDs {
+/*enum IDs {
 	BUTTON_ID = 2,
 	SLIDER_ID = 3,
 	TEXT_ID = 4
-};
+};*/
 
-wxBEGIN_EVENT_TABLE(MainFrame,wxFrame)
+/*wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
 	EVT_BUTTON(BUTTON_ID,MainFrame::OnButtonClicked)
 	EVT_SLIDER(SLIDER_ID,MainFrame::OnSliderChanged)
 	EVT_TEXT(TEXT_ID,MainFrame::OnTextChanged)
-wxEND_EVENT_TABLE()
+wxEND_EVENT_TABLE()*/
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 
 	wxPanel* panel = new wxPanel(this);
-	wxButton* button = new wxButton(panel,BUTTON_ID,"Button",wxPoint(300,275),wxSize(200,50));
-	wxSlider* slider = new wxSlider(panel,SLIDER_ID,0,0,100,wxPoint(300,200),wxSize(200,-1));
-	wxTextCtrl* text = new wxTextCtrl(panel,TEXT_ID,"",wxPoint(300,375),wxSize(200,-1));
+	wxButton* button = new wxButton(panel,wxID_ANY,"Button",wxPoint(300,275),wxSize(200,50));
+	wxSlider* slider = new wxSlider(panel, wxID_ANY,0,0,100,wxPoint(300,200),wxSize(200,-1));
+	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY,"",wxPoint(300,375),wxSize(200,-1));
+	
+	button->Bind(wxEVT_BUTTON,&MainFrame::OnButtonClicked,this);
+	slider->Bind(wxEVT_SLIDER,&MainFrame::OnSliderChanged,this);
+	text->Bind(wxEVT_TEXT,&MainFrame::OnTextChanged,this);
+
+	button->Unbind(wxEVT_BUTTON,&MainFrame::OnButtonClicked,this);
+
 	CreateStatusBar();
 
 	/*wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT);
