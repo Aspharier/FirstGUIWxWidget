2c232795c204c70cdd923627b7f6c4d0f02cfb1f
Aspharier <codeworkashish@gmail.com>
2024-06-27 18:57:32 +0530

    Added Event Propogation

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -17,15 +17,21 @@ wxEND_EVENT_TABLE()*/
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 
 	wxPanel* panel = new wxPanel(this);
-	wxButton* button = new wxButton(panel,wxID_ANY,"Button",wxPoint(300,275),wxSize(200,50));
-	wxSlider* slider = new wxSlider(panel, wxID_ANY,0,0,100,wxPoint(300,200),wxSize(200,-1));
-	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY,"",wxPoint(300,375),wxSize(200,-1));
+	wxButton* button1 = new wxButton(panel,wxID_ANY,"Button 1",wxPoint(300,275),wxSize(200,50));
+	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
+
+	/*wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
+	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY,"",wxPoint(300,375),wxSize(200,-1));*/
 	
-	button->Bind(wxEVT_BUTTON,&MainFrame::OnButtonClicked,this);
-	slider->Bind(wxEVT_SLIDER,&MainFrame::OnSliderChanged,this);
+	this->Bind(wxEVT_CLOSE_WINDOW,&MainFrame::OnClose,this);
+	this->Bind(wxEVT_BUTTON,&MainFrame::OnAnyButtonClicked,this);
+	button1->Bind(wxEVT_BUTTON,&MainFrame::OnButton1Clicked,this);
+	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
+
+	/*slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
 	text->Bind(wxEVT_TEXT,&MainFrame::OnTextChanged,this);
 
-	button->Unbind(wxEVT_BUTTON,&MainFrame::OnButtonClicked,this);
+	button->Unbind(wxEVT_BUTTON,&MainFrame::OnButtonClicked,this);*/
 
 	CreateStatusBar();
 
@@ -54,12 +60,24 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 
 	wxRadioBox* radiBox = new wxRadioBox(panel,wxID_ANY,"RadioBox",wxPoint(555,450),wxDefaultSize,choices,3,wxRA_SPECIFY_ROWS);*/
 }
-
-void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
-	wxLogStatus("Button Clicked");
+void MainFrame::OnClose(wxCloseEvent& evt) {
+	wxLogMessage("Frame Message");
+	evt.Skip();
 }
 
-void MainFrame::OnSliderChanged(wxCommandEvent& evt){
+void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt) {
+	wxLogMessage("Button Clicked");
+}
+
+void MainFrame::OnButton1Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 1 Clicked");
+	evt.Skip();
+}
+
+void MainFrame::OnButton2Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 2 Clicked");
+}
+/*void MainFrame::OnSliderChanged(wxCommandEvent& evt) {
 	wxString str = wxString::Format("Slider Value %d", evt.GetInt());
 	wxLogStatus(str);
 }
@@ -67,4 +85,4 @@ void MainFrame::OnSliderChanged(wxCommandEvent& evt){
 void MainFrame::OnTextChanged(wxCommandEvent& evt) {
 	wxString str = wxString::Format("Text %s", evt.GetString());
 	wxLogStatus(str);
-}
+}*/
--- MainFrame.h
+++ MainFrame.h
@@ -6,9 +6,12 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnButtonClicked(wxCommandEvent& evt);
-	void OnSliderChanged(wxCommandEvent& evt);
+	void OnAnyButtonClicked(wxCommandEvent& evt);
+	void OnButton1Clicked(wxCommandEvent& evt);
+	void OnButton2Clicked(wxCommandEvent& evt);
+	void OnClose(wxCloseEvent& evt);
+	/*void OnSliderChanged(wxCommandEvent& evt);
 	void OnTextChanged(wxCommandEvent& evt);
-	//wxDECLARE_EVENT_TABLE();
+	wxDECLARE_EVENT_TABLE();*/
 };
 
